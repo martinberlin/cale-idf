f786c9eea169a433d8f9e11e51f981789744f1ee
martinberlin <martin@luckycloud.de>
2020-06-18 21:38:21 +0200

    #6 Successful download wih correct content-length

--- main/cale.cpp
+++ main/cale.cpp
@@ -39,8 +39,7 @@ uint32_t countDataBytes=0;
 
 esp_err_t _http_event_handler(esp_http_client_event_t *evt)
 {
-    static char *output_buffer;  // Buffer to store response of http request from event handler
-    static int output_len;       // Stores number of bytes read
+    uint8_t output_buffer[512];  // Buffer to store response 
 
     switch(evt->event_id) {
         case HTTP_EVENT_ERROR:
@@ -56,53 +55,41 @@ esp_err_t _http_event_handler(esp_http_client_event_t *evt)
             ESP_LOGD(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
             break;
         case HTTP_EVENT_ON_DATA:
-             ++countDataEventCalls;
-             countDataBytes+=evt->data_len;
-
-             if (!esp_http_client_is_chunked_response(evt->client)) {
-                printf("\nD:%d Tlen:%d %.*s", countDataEventCalls, countDataBytes, evt->data_len, (char*)evt->data);
-            }
-            /* if (!esp_http_client_is_chunked_response(evt->client)) {
+            ++countDataEventCalls;
+            ESP_LOGI(TAG, "DATA calls:%d len:%d\n", countDataEventCalls, evt->data_len);
+            /*
+             *  Check for chunked encoding is added as the URL for chunked encoding used in this example returns binary data.
+             *  However, event handler can also be used in case chunked encoding is used.
+             */
+            if (!esp_http_client_is_chunked_response(evt->client)) {
                 // If user_data buffer is configured, copy the response into the buffer
-                if (evt->user_data) {
-                    memcpy(evt->user_data + output_len, evt->data, evt->data_len);
-                } else {
-                    if (output_buffer == NULL) {
-                        output_buffer = (char *) malloc(esp_http_client_get_content_length(evt->client));
-                        output_len = 0;
-                        if (output_buffer == NULL) {
-                            ESP_LOGE(TAG, "Failed to allocate memory for output buffer");
-                            return ESP_FAIL;
-                        }
-                    }
-                    memcpy(output_buffer + output_len, evt->data, evt->data_len);
-                }
-                output_len += evt->data_len;
-            } */
+               memcpy(output_buffer, evt->data, evt->data_len);
+
+               ESP_LOG_BUFFER_HEX(TAG, output_buffer, evt->data_len);
+
+            } else {
+                ESP_LOGI(TAG,"Is chunked response\n");
+            }
+
             break;
         case HTTP_EVENT_ON_FINISH:
-            ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
-            if (output_buffer != NULL) {
-                // Response is accumulated in output_buffer. Uncomment the below line to print the accumulated response
-                // ESP_LOG_BUFFER_HEX(TAG, output_buffer, output_len);
-                free(output_buffer);
-                output_buffer = NULL;
-                output_len = 0;
-            }
+            ESP_LOGI(TAG, "HTTP_EVENT_ON_FINISH\n");
+            
             break;
         case HTTP_EVENT_DISCONNECTED:
-            ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
-            
+            ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED\n");
+           
             break;
     }
     return ESP_OK;
 }
 
+
 static void http_post(void)
 {
     // w content len:  http://img.cale.es/jpg/fasani/5ea1dec401890
     // BMP :           http://img.cale.es/bmp/fasani/5e5926e25a985
-    char local_response_buffer[MAX_HTTP_OUTPUT_BUFFER] = {0};
+  
     /**
      * NOTE: All the configuration parameters for http_client must be spefied either in URL or as host and path parameters.
      * If host and path parameters are not set, query parameter will be ignored. In such cases,
@@ -115,10 +102,8 @@ static void http_post(void)
        2.13 http://img.cale.es/bmp/fasani/5e793990af85d -> 1st test
      */
     esp_http_client_config_t config = {
-        .host = "img.cale.es",
-        .path = "/bmp/fasani/5e793990af85d",
-        .event_handler = _http_event_handler,
-        .user_data = local_response_buffer,        // Pass address of local buffer to get response
+        .url = "http://img.cale.es/bmp/fasani/5e793990af85d",
+        .event_handler = _http_event_handler
     };
     esp_http_client_handle_t client = esp_http_client_init(&config);
 
@@ -131,7 +116,7 @@ static void http_post(void)
     } else {
         ESP_LOGE(TAG, "\nHTTP GET request failed: %s", esp_err_to_name(err));
     }
-    ESP_LOG_BUFFER_HEX(TAG, local_response_buffer, strlen(local_response_buffer));
+    //ESP_LOG_BUFFER_HEX(TAG, local_response_buffer, strlen(local_response_buffer));
 }
 
 
