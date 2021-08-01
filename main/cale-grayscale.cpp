639e21c680cc1c0cb0e3ba94bb20e04a95dca45e
martinberlin <martin@luckycloud.de>
2021-08-01 19:46:09 +0200

    #12 Reduce WiFi logging in cale-grayscale example

--- main/cale-grayscale.cpp
+++ main/cale-grayscale.cpp
@@ -688,6 +688,8 @@ void app_main(void)
     }
     ESP_ERROR_CHECK(ret);
 
+    // WiFi log level
+    esp_log_level_set("wifi", ESP_LOG_ERROR);
     ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
     wifi_init_sta();
     
