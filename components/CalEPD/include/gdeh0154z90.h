b8582d8f36dc01ab304b06b24b9b3c7deb1742a6
Benjamin Stürmer <webmaster@stuermer-benjamin.de>
2021-09-01 21:20:44 +0200

    #47 add support for gdeh0154z90

---
+++ components/CalEPD/include/gdeh0154z90.h
@@ -0,0 +1,65 @@
+// Contributed by thebino: https://github.com/martinberlin/cale-idf/issues/47
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "esp_system.h"
+#include <stdint.h>
+#include <math.h>
+#include "sdkconfig.h"
+#include "esp_log.h"
+#include <string>
+#include <epd.h>
+#include <Adafruit_GFX.h>
+#include <epdspi.h>
+#include <gdew_colors.h>
+
+// Controller: SSD1681  Note: Used by Waveshare in https://www.waveshare.com/wiki/1.54inch_e-Paper_Module_(B)
+#define GDEH0154Z90_WIDTH 200
+#define GDEH0154Z90_HEIGHT 200
+#define GDEH0154Z90_BUFFER_SIZE (uint32_t(GDEH0154Z90_WIDTH) * uint32_t(GDEH0154Z90_HEIGHT) / 8)
+// 1 byte of this color in the buffer
+#define GDEH0154Z90_8PIX_BLACK 0xFF
+#define GDEH0154Z90_8PIX_WHITE 0x00
+
+class Gdeh0154z90 : public Epd
+{
+  public:
+    Gdeh0154z90(EpdSpi& IO);
+    uint8_t colors_supported = 1;
+    bool _initial_refresh = false;
+    bool _using_partial_mode = false;
+    
+    static const uint16_t power_on_time = 100; // ms, e.g. 95583us
+    static const uint16_t power_off_time = 150; // ms, e.g. 140621us
+    static const uint16_t full_refresh_time = 2600; // ms, e.g. 2509602us
+    static const uint16_t partial_refresh_time = 500; // ms, e.g. 457282us
+
+    void init(bool debug);
+    void initFullUpdate();
+    void initPartialUpdate();
+    void drawPixel(int16_t x, int16_t y, uint16_t color);  // Override GFX own drawPixel method
+    void fillScreen(uint16_t color);
+    void update();
+    // Partial update of rectangle from buffer to screen, does not power off
+    void updateWindow(int16_t x, int16_t y, int16_t w, int16_t h, bool using_rotation = true);
+
+  private:
+    EpdSpi& IO;
+    uint8_t _buffer[GDEH0154Z90_BUFFER_SIZE];
+    bool color = false;
+    bool _initial = true;
+    bool _partial_mode = false;
+    bool _debug_buffer = false;
+    void _PowerOn();
+    void _setRamDataEntryMode(uint8_t em);
+    void _SetRamArea(uint8_t Xstart, uint8_t Xend, uint8_t Ystart, uint8_t Ystart1, uint8_t Yend, uint8_t Yend1);
+    void _SetRamPointer(uint8_t addrX, uint8_t addrY, uint8_t addrY1);
+
+    void _setPartialRamArea(uint16_t x, uint16_t y, uint16_t xe, uint16_t ye);
+    void _wakeUp();
+    void _sleep();
+    void _waitBusy(const char* message, uint16_t busy_time);
+    void _waitBusy(const char* message);
+    void _rotate(int16_t& x, int16_t& y, int16_t& w, int16_t& h);
+};
---
+++ components/CalEPD/models/gdeh0154z90.cpp
@@ -0,0 +1,412 @@
+#include "gdeh0154z90.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "esp_log.h"
+#include "freertos/task.h"
+
+// Partial Update Delay
+#define GDEH0154Z90_PU_DELAY 300
+
+// Constructor
+Gdeh0154z90::Gdeh0154z90(EpdSpi &dio) : Adafruit_GFX(GDEH0154Z90_WIDTH, GDEH0154Z90_HEIGHT),
+                                        Epd(GDEH0154Z90_WIDTH, GDEH0154Z90_HEIGHT), IO(dio)
+{
+    ESP_LOGI(TAG, "Gdeh0154z90() %d*%d\n", GDEH0154Z90_WIDTH, GDEH0154Z90_HEIGHT);
+}
+
+void Gdeh0154z90::initFullUpdate()
+{
+    _wakeUp();
+    _partial_mode = false;
+    _PowerOn();
+    if (debug_enabled)
+    {
+        ESP_LOGI(TAG, "initFullUpdate()\n");
+    }
+}
+
+void Gdeh0154z90::initPartialUpdate()
+{
+    _partial_mode = true;
+    _wakeUp();
+    _PowerOn();
+    if (debug_enabled)
+    {
+        ESP_LOGI(TAG, "initPartialUpdate()\n");
+    }
+}
+
+//Initialize the display
+void Gdeh0154z90::init(bool debug)
+{
+    debug_enabled = debug;
+    if (debug_enabled)
+    {
+        ESP_LOGI(TAG, "Gdeh0154z90::init(%d)\n", debug);
+    }
+    IO.init(4, debug); // 4MHz frequency
+
+    ESP_LOGI(TAG, "Free heap:%d\n", xPortGetFreeHeapSize());
+    fillScreen(EPD_WHITE);
+}
+
+void Gdeh0154z90::fillScreen(uint16_t color)
+{
+    // 0xFF = 8 pixels black, 0x00 = 8 pix. white
+    uint8_t data = (color == EPD_BLACK) ? GDEH0154Z90_8PIX_BLACK : GDEH0154Z90_8PIX_WHITE;
+    for (uint16_t x = 0; x < sizeof(_buffer); x++)
+    {
+        _buffer[x] = data;
+    }
+
+    if (debug_enabled)
+    {
+        ESP_LOGI(TAG, "fillScreen(%d) _buffer len:%d\n", data, sizeof(_buffer));
+    }
+}
+
+void Gdeh0154z90::_setPartialRamArea(uint16_t x, uint16_t y, uint16_t w, uint16_t h)
+{
+    IO.cmd(0x11);  // set ram entry mode
+    IO.data(0x03); // x increase, y increase : normal mode
+
+    IO.cmd(0x44);
+    IO.data(x / 8);
+    IO.data((x + w - 1) / 8);
+
+    IO.cmd(0x45);
+    IO.data(y % 256);
+    IO.data(y / 256);
+    IO.data((y + h - 1) % 256);
+    IO.data((y + h - 1) / 256);
+
+    IO.cmd(0x4e);
+    IO.data(x / 8);
+
+    IO.cmd(0x4f);
+    IO.data(y % 256);
+    IO.data(y / 256);
+}
+
+void Gdeh0154z90::_wakeUp()
+{
+    IO.cmd(0x12); // SWRESET
+    _waitBusy("epd_wakeup_power:ON", power_on_time);
+    IO.cmd(0x01); // Driver output control
+    IO.data(0xC7);
+    IO.data(0x00);
+    IO.data(0x00);
+
+    //
+    // TODO: check following block
+    //
+    IO.cmd(0x11); //data entry mode
+    IO.data(0x01);
+
+    IO.cmd(0x44); //set Ram-X address start/end position
+    IO.data(0x00);
+    IO.data(0x18); //0x18-->(24+1)*8=200
+
+    IO.cmd(0x45);  //set Ram-Y address start/end position
+    IO.data(0xC7); //0xC7-->(199+1)=200
+    IO.data(0x00);
+    IO.data(0x00);
+    IO.data(0x00);
+    //
+    // TODO: end
+    //
+
+
+    IO.cmd(0x3C); // BorderWavefrom
+    IO.data(0x05);
+    IO.cmd(0x18); // Read built-in temperature sensor
+    IO.data(0x80);
+
+    _setRamDataEntryMode(0x03);
+}
+
+void Gdeh0154z90::update()
+{
+    uint64_t startTime = esp_timer_get_time();
+    initFullUpdate();
+    _using_partial_mode = false;
+    _initial_refresh = true;
+    ESP_LOGI(TAG, "BUFF Size:%d\n", sizeof(_buffer));
+
+    IO.cmd(0x24); // update current data
+    for (uint16_t y = 0; y < GDEH0154Z90_HEIGHT; y++)
+    {
+        for (uint16_t x = 0; x < GDEH0154Z90_WIDTH / 8; x++)
+        {
+            uint16_t idx = y * (GDEH0154Z90_WIDTH / 8) + x;
+            uint8_t data = (idx < sizeof(_buffer)) ? _buffer[idx] : 0x00;
+            IO.data(~data);
+        }
+    }
+    uint64_t endTime = esp_timer_get_time();
+
+    IO.cmd(0x22);
+    IO.data(0xf7);
+    IO.cmd(0x20);
+    _waitBusy("_Update_Full", full_refresh_time);
+
+    uint64_t updateTime = esp_timer_get_time();
+
+    ESP_LOGI(TAG, "\n\nSTATS (ms)\n%llu _wakeUp settings+send Buffer\n%llu update \n%llu total time in millis\n",
+             (endTime - startTime) / 1000, (updateTime - endTime) / 1000, (updateTime - startTime) / 1000);
+
+    _sleep();
+}
+
+void Gdeh0154z90::_setRamDataEntryMode(uint8_t em)
+{
+    const uint16_t xPixelsPar = GDEH0154Z90_WIDTH - 1;
+    const uint16_t yPixelsPar = GDEH0154Z90_HEIGHT - 1;
+    em = gx_uint16_min(em, 0x03);
+    IO.cmd(0x11);
+    IO.data(em);
+
+    switch (em)
+    {
+    case 0x00:                                                                             // x decrease, y decrease
+        _SetRamArea(xPixelsPar / 8, 0x00, yPixelsPar % 256, yPixelsPar / 256, 0x00, 0x00); // X-source area,Y-gate area
+        _SetRamPointer(xPixelsPar / 8, yPixelsPar % 256, yPixelsPar / 256);                // set ram
+        break;
+    case 0x01:                                                                             // x increase, y decrease : as in demo code
+        _SetRamArea(0x00, xPixelsPar / 8, yPixelsPar % 256, yPixelsPar / 256, 0x00, 0x00); // X-source area,Y-gate area
+        _SetRamPointer(0x00, yPixelsPar % 256, yPixelsPar / 256);                          // set ram
+        break;
+    case 0x02:                                                                             // x decrease, y increase
+        _SetRamArea(xPixelsPar / 8, 0x00, 0x00, 0x00, yPixelsPar % 256, yPixelsPar / 256); // X-source area,Y-gate area
+        _SetRamPointer(xPixelsPar / 8, 0x00, 0x00);                                        // set ram
+        break;
+    case 0x03:                                                                             // x increase, y increase : normal mode
+        _SetRamArea(0x00, xPixelsPar / 8, 0x00, 0x00, yPixelsPar % 256, yPixelsPar / 256); // X-source area,Y-gate area
+        _SetRamPointer(0x00, 0x00, 0x00);                                                  // set ram
+        break;
+    }
+}
+
+void Gdeh0154z90::_SetRamArea(uint8_t Xstart, uint8_t Xend, uint8_t Ystart, uint8_t Ystart1, uint8_t Yend, uint8_t Yend1)
+{
+    IO.cmd(0x44);
+    IO.data(Xstart);
+    IO.data(Xend);
+    IO.cmd(0x45);
+    IO.data(Ystart);
+    IO.data(Ystart1);
+    IO.data(Yend);
+    IO.data(Yend1);
+}
+
+void Gdeh0154z90::_SetRamPointer(uint8_t addrX, uint8_t addrY, uint8_t addrY1)
+{
+    IO.cmd(0x4e);
+    IO.data(addrX);
+    IO.cmd(0x4f);
+    IO.data(addrY);
+    IO.data(addrY1);
+}
+
+void Gdeh0154z90::_PowerOn(void)
+{
+    IO.cmd(0x22);
+    IO.data(0xc0);
+    IO.cmd(0x20);
+    _waitBusy("_PowerOn", power_on_time);
+}
+
+void Gdeh0154z90::updateWindow(int16_t x, int16_t y, int16_t w, int16_t h, bool using_rotation)
+{
+    if (using_rotation)
+        _rotate(x, y, w, h);
+    if (x >= WIDTH)
+    {
+        ESP_LOGD(TAG, "x:%d exceeded boundary %d\n", x, WIDTH);
+        return;
+    }
+    if (y >= HEIGHT)
+    {
+        ESP_LOGD(TAG, "y:%d exceeded boundary %d\n", y, HEIGHT);
+        return;
+    }
+
+    if (!_initial_refresh)
+    {
+        ESP_LOGD(TAG, "updateWindow() doing initial refresh\n");
+        update();
+    }
+    uint64_t startTime = esp_timer_get_time();
+
+    uint16_t xe = gx_uint16_min(GDEH0154Z90_WIDTH, x + w) - 1;
+    uint16_t ye = gx_uint16_min(GDEH0154Z90_HEIGHT, y + h) - 1;
+    uint16_t xs_d8 = x / 8;
+    uint16_t xe_d8 = xe / 8;
+
+    initPartialUpdate();
+
+    _SetRamArea(xs_d8, xe_d8, y % 256, y / 256, ye % 256, ye / 256); // X-source area,Y-gate area
+    _SetRamPointer(xs_d8, y % 256, y / 256);                         // set ram
+    _waitBusy("ram_pointer1", 100);
+    IO.cmd(0x24);
+
+    for (int16_t y1 = y; y1 <= ye; y1++)
+    {
+        for (int16_t x1 = xs_d8; x1 <= xe_d8; x1++)
+        {
+            uint16_t idx = y1 * (WIDTH / 8) + x1;
+            uint8_t data = (idx < sizeof(_buffer)) ? _buffer[idx] : 0x00;
+            IO.data(~data);
+        }
+    }
+
+    uint64_t endTime = esp_timer_get_time();
+
+    // Update partial
+    IO.cmd(0x22);
+    IO.data(0xff);
+    IO.cmd(0x20);
+    _waitBusy("partial_update", 100);
+    uint64_t updateTime = esp_timer_get_time();
+
+    // Clean buffer: 0x01 is essential
+    _setRamDataEntryMode(0x01);
+    IO.cmd(0x24);
+
+    uint8_t xLineBytes = GDEH0154Z90_WIDTH / 8;
+    uint8_t x1cbuf[xLineBytes];
+    for (uint16_t y = 1; y <= GDEH0154Z90_HEIGHT; y++)
+    {
+        for (uint16_t x = 1; x <= xLineBytes; x++)
+        {
+            x1cbuf[x - 1] = 0xFF;
+        }
+        IO.data(x1cbuf, sizeof(x1cbuf));
+    }
+
+    if (debug_enabled)
+    {
+        uint64_t cleanTime = esp_timer_get_time();
+        ESP_LOGI(TAG, "\n\nSTATS (ms)\n%llu _wakeUp settings+send Buffer\n%llu update \nclean_buffer:%llu\n%llu total time in millis\n",
+                 (endTime - startTime) / 1000, (updateTime - endTime) / 1000, (cleanTime - updateTime) / 1000, (cleanTime - startTime) / 1000);
+    }
+}
+
+void Gdeh0154z90::_waitBusy(const char *message, uint16_t busy_time)
+{
+    if (debug_enabled)
+    {
+        ESP_LOGI(TAG, "_waitBusy for %s", message);
+    }
+    int64_t time_since_boot = esp_timer_get_time();
+    // On high is busy
+    if (gpio_get_level((gpio_num_t)CONFIG_EINK_BUSY) == 1)
+    {
+        while (1)
+        {
+            if (gpio_get_level((gpio_num_t)CONFIG_EINK_BUSY) == 0)
+                break;
+            vTaskDelay(1);
+            if (esp_timer_get_time() - time_since_boot > 7000000)
+            {
+                if (debug_enabled)
+                {
+                    ESP_LOGI(TAG, "Busy Timeout");
+                }
+                break;
+            }
+        }
+    }
+    else
+    {
+        vTaskDelay(busy_time / portTICK_RATE_MS);
+    }
+}
+
+void Gdeh0154z90::_waitBusy(const char *message)
+{
+    if (debug_enabled)
+    {
+        ESP_LOGI(TAG, "_waitBusy for %s", message);
+    }
+    int64_t time_since_boot = esp_timer_get_time();
+
+    while (1)
+    {
+        // On low is not busy anymore
+        if (gpio_get_level((gpio_num_t)CONFIG_EINK_BUSY) == 0)
+            break;
+        vTaskDelay(1);
+        if (esp_timer_get_time() - time_since_boot > 7000000)
+        {
+            if (debug_enabled)
+            {
+                ESP_LOGI(TAG, "Busy Timeout");
+            }
+            break;
+        }
+    }
+}
+
+void Gdeh0154z90::_sleep()
+{
+    IO.cmd(0x22); // power off display
+    IO.data(0xc3);
+    IO.cmd(0x20);
+    _waitBusy("power_off", power_off_time);
+}
+
+void Gdeh0154z90::_rotate(int16_t &x, int16_t &y, int16_t &w, int16_t &h)
+{
+    switch (getRotation())
+    {
+    case 1:
+        swap(x, y);
+        swap(w, h);
+        x = GDEH0154Z90_WIDTH - x - w - 1;
+        break;
+    case 2:
+        x = GDEH0154Z90_WIDTH - x - w - 1;
+        y = GDEH0154Z90_HEIGHT - y - h - 1;
+        break;
+    case 3:
+        swap(x, y);
+        swap(w, h);
+        y = GDEH0154Z90_HEIGHT - y - h - 1;
+        break;
+    }
+}
+
+void Gdeh0154z90::drawPixel(int16_t x, int16_t y, uint16_t color)
+{
+    if ((x < 0) || (x >= width()) || (y < 0) || (y >= height()))
+        return;
+
+    // check rotation, move pixel around if necessary
+    switch (getRotation())
+    {
+    case 1:
+        swap(x, y);
+        x = GDEH0154Z90_WIDTH - x - 1;
+        break;
+    case 2:
+        x = GDEH0154Z90_WIDTH - x - 1;
+        y = GDEH0154Z90_HEIGHT - y - 1;
+        break;
+    case 3:
+        swap(x, y);
+        y = GDEH0154Z90_HEIGHT - y - 1;
+        break;
+    }
+    uint16_t i = x / 8 + y * GDEH0154Z90_WIDTH / 8;
+
+    // This is the trick to draw colors right. Genious Jean-Marc
+    if (color)
+    {
+        _buffer[i] = (_buffer[i] & (0xFF ^ (1 << (7 - x % 8))));
+    }
+    else
+    {
+        _buffer[i] = (_buffer[i] | (1 << (7 - x % 8)));
+    }
+}
