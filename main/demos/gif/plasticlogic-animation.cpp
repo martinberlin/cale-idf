a3d118909348fd74623e2318a0dfee7fa6c74d4f
martinberlin <martin@luckycloud.de>
2022-01-05 23:33:43 +0100

    GIF animation running, transparence not correct yet

--- main/demos/gif/plasticlogic-animation.cpp
+++ main/demos/gif/plasticlogic-animation.cpp
@@ -9,24 +9,16 @@
 #include "esp_timer.h"
 #include <AnimatedGIF.h>
 // Digit animated GIFs
-#include <digit_0.h>
-#include <digit_1.h>
-#include <digit_2.h>
-#include <digit_3.h>
-#include <digit_4.h>
-#include <digit_5.h>
-#include <digit_6.h>
-#include <digit_7.h>
-#include <digit_8.h>
-#include <digit_9.h>
-
-const uint8_t * digits[10] = {digit_0, digit_1, digit_2, digit_3, digit_4, digit_5, digit_6, digit_7, digit_8, digit_9};
-const size_t lengths[10] = {sizeof(digit_0), sizeof(digit_1), sizeof(digit_2), sizeof(digit_3), sizeof(digit_4), sizeof(digit_5), sizeof(digit_6), sizeof(digit_7), sizeof(digit_8), sizeof(digit_9)};
+#include <homer.h>
+#include <anime.h>
+// Replace here the variable name in the GIF.h above
+uint8_t * gif_array = (uint8_t *) anime;
+uint32_t gif_array_size = sizeof(anime);
 
 // Should match with your epaper module and size
 // One or many classes can be included at the same time
-//#include <plasticlogic011.h>
-//#include <plasticlogic014.h>
+#include <plasticlogic011.h>
+#include <plasticlogic014.h>
 #include <plasticlogic021.h>
 // Plasticlogic EPD should implement EpdSpi2Cs Full duplex SPI
 EpdSpi2Cs io;
@@ -34,19 +26,13 @@ EpdSpi2Cs io;
 //PlasticLogic014 display(io);
 PlasticLogic021 display(io);
 bool playShortDemo = true;
-// Need 4 independent instances of the class to animate the 4 digits simultaneously
-AnimatedGIF gif[4]; 
+
+AnimatedGIF gif; 
 
 extern "C"
 {
    void app_main();
 }
-
-typedef struct my_private_struct
-{
-  int xoff, yoff; // corner offset
-} PRIVATE;
-
 // IDF function to emulate Arduino's millis()
 uint32_t millis() {
    return esp_timer_get_time()/1000;
@@ -64,16 +50,17 @@ void GIFDraw(GIFDRAW *pDraw)
     uint8_t *s;
     uint16_t *d, *usPalette, usTemp[display.width()];
     int x, y, iWidth;
-    PRIVATE *pPriv = (PRIVATE *)pDraw->pUser;
-    
+
     iWidth = pDraw->iWidth;
     if (iWidth + pDraw->iX > display.width())
        iWidth = display.width() - pDraw->iX;
     usPalette = pDraw->pPalette;
-    y = pPriv->yoff + pDraw->iY + pDraw->y; // current line
+
+    y =  pDraw->y; // current line
     if (y >= display.height() || pDraw->iX >= display.width() || iWidth < 1)
        return; 
     s = pDraw->pPixels;
+
     if (pDraw->ucDisposalMethod == 2) // restore to background color
     {
       for (x=0; x<iWidth; x++)
@@ -135,15 +122,12 @@ void GIFDraw(GIFDRAW *pDraw)
             }
             
             //printf("%d ", color); // W the fuckio!
-            display.drawPixel(pPriv->xoff + pDraw->iX + x + c, y, color);
+            display.drawPixel(x, y, color);
           }
-
-          //tft.setAddrWindow(pPriv->xoff + pDraw->iX + x, y, iCount, 1);
           x += iCount;
           iCount = 0;
         }
 
-        
         // no, look for a run of transparent pixels
         c = ucTransparent;
         while (c == ucTransparent && s < pEnd)
@@ -162,67 +146,8 @@ void GIFDraw(GIFDRAW *pDraw)
       }
       
     }
-    else
-    {
-      s = pDraw->pPixels;
-      // Translate the 8-bit pixels through the RGB565 palette (already byte reversed)
-      for (x=0; x<iWidth; x++) {
-        usTemp[x] = usPalette[*s++];
-        printf("N%d ", usTemp[x]);
-        }
-
-      /* tft.startWrite();
-      tft.setAddrWindow(pPriv->xoff + pDraw->iX, y, iWidth, 1);
-      tft.writePixels(usTemp, iWidth, false, false);
-      tft.endWrite(); */
-    }
+    
 }
-
-//
-// Display 4 animated digits (80 pixels wide each)
-// only animate the digits which change from one iteration to the next
-//
-void ShowDigits(int iValue, int iOldValue)
-{
-int i, rc, iBusy;
-PRIVATE priv;
-int jn, jo, t0, t1;
-
-  priv.yoff = 72; // center digits vertically  
-  iBusy = 0;
-  // mark digitis which need to change with a single bit flag
-  jn = iValue; jo = iOldValue;
-  for (i=3; i>=0; i--) { // compare old and new values
-    t0 = jn % 10; t1 = jo % 10;
-    if (t0 != t1) {
-      iBusy |= (1 << i);
-      gif[i].open((uint8_t *)digits[t0], lengths[t0], GIFDraw); // prepare the right digit animated file
-    }
-    jn /= 10;
-    jo /= 10; // next digit
-  }
- 
-  i = 1; // Remove if FOR is enabled
-  while (iBusy) {
-    // Draw each frame of each changing digit together so that they animate together
-    lTime = millis() + 40; // play the frames at a rate of 25fps (40ms per frame)
-    //for (i=0; i<4; i++) {
-     //  if (iBusy & (1 << i)) {
-         // Advance this animation one frame
-         priv.xoff = 80 * i; // each digit is 80 pixels wide
-         rc = gif[i].playFrame(false, NULL, (void *)&priv); // draw it and return immediately
-         if (!rc) { // animation has ended
-            iBusy &= ~(1<<i); // clear the bit indicating this digit is busy
-            gif[i].close();
-    /*      }
-       } */
-    } // for each digit
-    display.update(EPD_UPD_PART);
-    display.fillScreen(EPD_WHITE);
-    vTaskDelay(150 / portTICK_PERIOD_MS);
-  } // while digits still changing
-}
-
 
 void app_main(void)
 {
@@ -237,19 +162,20 @@ void app_main(void)
    
    // Initialize display class
    display.init();         // Add init(true) for debug
+  gif.begin(GIF_PALETTE_RGB888);
 
-   // TODO: Rotation is not working as it should:
-   //display.setEpdRotation(2); // 2: Does not turn it portrait, just upside down (Same with Paperino PL_microEPD)
-   
-   for (int i=0; i<4; i++) {
-      // GIF_PALETTE_RGB565_LE (Also another possibility)
-     gif[i].begin(GIF_PALETTE_RGB888);
-   }
-
-  uint16_t i = 0;
-  uint16_t iOld = 9999; // force every digit to be drawn the first time
-
-  ShowDigits(i, iOld);
-
+  int rc;
+  rc = gif.open(gif_array, gif_array_size, GIFDraw);
+  
+  if (rc) {
+    while (rc) {
+      rc = gif.playFrame(true, NULL); // play a frame and pause for the correct amount of time
+      display.update(EPD_UPD_PART);
+      vTaskDelay(20 / portTICK_PERIOD_MS);
+      display.fillScreen(EPD_WHITE);
+    }
+    gif.close();
+  }
+  
 
 }
