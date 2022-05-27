9f1d1f3db7e0a7382c8b87646275b3b6a768f31d
martinberlin <martin@luckycloud.de>
2022-05-28 01:24:05 +0200

    Testing CINREAD 9.7" parallel SPI with LovyanGFX

---
+++ main/www-jpg-render/main/jpg-render-cinread8951.cpp
@@ -0,0 +1,517 @@
+// Note: Run menuconfig to set the WiFi Credentials -> CALE Configuration
+// Requirements: Needs to have an EPD Class defined. Needs PSRAM.
+// This example does not use a decoded buffer hence leaves more external RAM free
+// and it uses a different JPEG decoder: https://github.com/bitbank2/JPEGDEC
+// as an ESP-IDF "component"
+#include "esp_heap_caps.h"
+#include "esp_log.h"
+#include "esp_timer.h"
+#include "esp_types.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_task_wdt.h"
+#include "esp_sleep.h"
+// WiFi related
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "lwip/err.h"
+#include "lwip/sys.h"
+// HTTP Client + time
+#include "esp_netif.h"
+#include "esp_http_client.h"
+#include "esp_sntp.h"
+// C
+#include <stdio.h>
+#include <string.h>
+#include <math.h> // round + pow
+// JPG decoder from @bitbank2
+#include "JPEGDEC.h"
+// - - - - Display configuration - - - - - - - - -
+#define EPD_WIDTH  1200
+#define EPD_HEIGHT 825
+#define IMG_URL "http://img.cale.es/jpg/fasani/5e636b0f39aac"
+#define HTTP_RECEIVE_BUFFER_SIZE 1024
+
+// Load the EMBED_TXTFILES. Then doing (char*) server_cert_pem_start you get the SSL certificate
+// Reference: https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-guides/build-system.html#embedding-binary-data
+extern const uint8_t server_cert_pem_start[] asm("_binary_server_cert_pem_start");
+
+
+JPEGDEC jpeg;
+
+// Dither space allocation
+uint8_t * dither_space;
+
+// Affects the gamma to calculate gray (lower is darker/higher contrast)
+// Nice test values: 0.9 1.2 1.4 higher and is too bright
+double gamma_value = 0.7;
+// Internal array for gamma grayscale
+uint8_t gamme_curve[256];
+#define LGFX_USE_V1
+#include <LovyanGFX.hpp>
+
+class LGFX : public lgfx::LGFX_Device
+{
+  lgfx::Panel_IT8951   _panel_instance;
+  lgfx::Bus_SPI       _bus_instance; 
+  lgfx::Light_PWM     _light_instance;
+
+public:
+
+  // コンストラクタを作成し、ここで各種設定を行います。
+  // クラス名を変更した場合はコンストラクタも同じ名前を指定してください。
+  LGFX(void)
+  {
+    { // バス制御の設定を行います。
+      auto cfg = _bus_instance.config();    // バス設定用の構造体を取得します。
+
+// SPIバスの設定
+      cfg.spi_host = SPI2_HOST;     // 使用するSPIを選択  (VSPI_HOST or HSPI_HOST)
+      cfg.spi_mode = 0;             // SPI通信モードを設定 (0 ~ 3)
+      cfg.freq_write = 20000000;    // 送信時のSPIクロック (最大80MHz, 80MHzを整数で割った値に丸められます)
+      cfg.freq_read  = 16000000;    // 受信時のSPIクロック
+      cfg.spi_3wire  = true;        // 受信をMOSIピンで行う場合はtrueを設定
+      cfg.use_lock   = true;        // トランザクションロックを使用する場合はtrueを設定
+      cfg.dma_channel = 1;          // Set the DMA channel (1 or 2. 0=disable)   使用するDMAチャンネルを設定 (0=DMA不使用)
+      cfg.pin_sclk = CONFIG_EINK_SPI_CLK;            // SPIのSCLKピン番号を設定
+      cfg.pin_mosi = CONFIG_EINK_SPI_MOSI;           // SPIのMOSIピン番号を設定
+      cfg.pin_miso = CONFIG_EINK_SPI_MISO;           // SPIのMISOピン番号を設定 (-1 = disable)
+      cfg.pin_dc   = -1;            // SPIのD/Cピン番号を設定  (-1 = disable)
+ 
+      _bus_instance.config(cfg);    // 設定値をバスに反映します。
+      _panel_instance.setBus(&_bus_instance);      // バスをパネルにセットします。
+    }
+
+    { // 表示パネル制御の設定を行います。
+      auto cfg = _panel_instance.config();    // 表示パネル設定用の構造体を取得します。
+
+      cfg.pin_cs           =    CONFIG_EINK_SPI_CS;  // CSが接続されているピン番号   (-1 = disable)
+      cfg.pin_rst          =    -1;
+      cfg.pin_busy         =    CONFIG_EINK_BUSY;    // BUSYが接続されているピン番号 (-1 = disable)
+
+      // ※ 以下の設定値はパネル毎に一般的な初期値が設定されていますので、不明な項目はコメントアウトして試してみてください。
+
+      cfg.memory_width     =   EPD_WIDTH;  // ドライバICがサポートしている最大の幅
+      cfg.memory_height    =   EPD_HEIGHT;  // ドライバICがサポートしている最大の高さ
+      cfg.panel_width      =   EPD_WIDTH;  // 実際に表示可能な幅
+      cfg.panel_height     =   EPD_HEIGHT;  // 実際に表示可能な高さ
+      cfg.offset_x         =     0;  // パネルのX方向オフセット量
+      cfg.offset_y         =     0;  // パネルのY方向オフセット量
+      cfg.offset_rotation  =     0;  // 回転方向の値のオフセット 0~7 (4~7は上下反転)
+      cfg.dummy_read_pixel =     8;  // ピクセル読出し前のダミーリードのビット数
+      cfg.dummy_read_bits  =     1;  // ピクセル以外のデータ読出し前のダミーリードのビット数
+      cfg.readable         =  true;  // データ読出しが可能な場合 trueに設定
+      cfg.invert           =  true;  // パネルの明暗が反転してしまう場合 trueに設定
+      cfg.rgb_order        = false;  // パネルの赤と青が入れ替わってしまう場合 trueに設定
+      cfg.dlen_16bit       = false;  // データ長を16bit単位で送信するパネルの場合 trueに設定
+      cfg.bus_shared       =  true;  // SDカードとバスを共有している場合 trueに設定(drawJpgFile等でバス制御を行います)
+
+      _panel_instance.config(cfg);
+    }
+
+    setPanel(&_panel_instance); // 使用するパネルをセットします。
+  }
+};
+
+// 準備したクラスのインスタンスを作成します。
+LGFX display;
+// - - - - end of Display configuration  - - - - -
+
+extern "C"
+{
+    void app_main();
+}
+
+#define DEBUG_VERBOSE true
+
+// Buffers
+uint8_t *fb;            // EPD 2bpp buffer
+uint8_t *source_buf;    // JPG download buffer
+
+uint32_t buffer_pos = 0;
+uint32_t time_download = 0;
+uint32_t time_decomp = 0;
+uint32_t time_render = 0;
+uint16_t ep_width = 0;
+uint16_t ep_height = 0;
+
+static const char *TAG = "Jpgdec";
+uint16_t countDataEventCalls = 0;
+uint32_t countDataBytes = 0;
+uint32_t img_buf_pos = 0;
+uint64_t startTime = 0;
+
+
+#if VALIDATE_SSL_CERTIFICATE == true
+  /* Time aware for ESP32: Important to check SSL certs validity */
+  void time_sync_notification_cb(struct timeval *tv)
+  {
+      ESP_LOGI(TAG, "Notification of a time synchronization event");
+  }
+
+  static void initialize_sntp(void)
+  {
+      ESP_LOGI(TAG, "Initializing SNTP");
+      sntp_setoperatingmode(SNTP_OPMODE_POLL);
+      sntp_setservername(0, "pool.ntp.org");
+      sntp_set_time_sync_notification_cb(time_sync_notification_cb);
+  #ifdef CONFIG_SNTP_TIME_SYNC_METHOD_SMOOTH
+      sntp_set_sync_mode(SNTP_SYNC_MODE_SMOOTH);
+  #endif
+      sntp_init();
+  }
+
+  static void obtain_time(void)
+  {
+      initialize_sntp();
+
+      // wait for time to be set
+      time_t now = 0;
+      struct tm timeinfo = { 0 };
+      int retry = 0;
+      const int retry_count = 10;
+      while (sntp_get_sync_status() == SNTP_SYNC_STATUS_RESET && ++retry < retry_count) {
+          ESP_LOGI(TAG, "Waiting for system time to be set... (%d/%d)", retry, retry_count);
+          vTaskDelay(2000 / portTICK_PERIOD_MS);
+      }
+      time(&now);
+      localtime_r(&now, &timeinfo);
+  }
+#endif
+//====================================================================================
+// This sketch contains support functions to render the Jpeg images
+//
+// Created by Bitbank
+// Refactored by @martinberlin for EPDiy as a Jpeg download and render example
+//====================================================================================
+
+/*
+ * Used with jpeg.setPixelType(FOUR_BIT_DITHERED)
+ */
+uint16_t mcu_count = 0;
+int JPEGDraw4Bits(JPEGDRAW *pDraw)
+{
+  uint32_t render_start = esp_timer_get_time();
+
+    // Rotation aware
+    /* for (int16_t xx = 0; xx < pDraw->iWidth; xx+=4) {
+      for (int16_t yy = 0; yy < pDraw->iHeight; yy++) {
+        uint16_t col = pDraw->pPixels[ (xx + (yy * pDraw->iWidth)) >>2 ];
+      
+        uint8_t col1 = col & 0xf;
+        uint8_t col2 = (col >> 4) & 0xf;
+        uint8_t col3 = (col >> 8) & 0xf;
+        uint8_t col4 = (col >> 12) & 0xf;
+        display.drawPixel(pDraw->x + xx, pDraw->y + yy, col1 );
+        display.drawPixel(pDraw->x + xx + 1, pDraw->y + yy, col2);
+        display.drawPixel(pDraw->x + xx + 2, pDraw->y + yy, col3);
+        display.drawPixel(pDraw->x + xx + 3, pDraw->y + yy, col4);
+      }
+    }  */
+  
+
+   display.pushImage(pDraw->x , pDraw->y, pDraw->iWidth, pDraw->iHeight, (lgfx::grayscale_t*)pDraw->pPixels);
+
+  mcu_count++;
+  time_render += (esp_timer_get_time() - render_start) / 1000;
+  return 1;
+}
+
+void deepsleep(){
+    esp_deep_sleep(1000000LL * 60 * CONFIG_DEEPSLEEP_MINUTES_AFTER_RENDER);
+}
+
+//====================================================================================
+//   This function opens source_buf Jpeg image file and primes the decoder
+//====================================================================================
+int decodeJpeg(uint8_t *source_buf, int xpos, int ypos) {
+  uint32_t decode_start = esp_timer_get_time();
+
+  if (jpeg.openRAM(source_buf, img_buf_pos, JPEGDraw4Bits)) {
+
+    jpeg.setPixelType(FOUR_BIT_DITHERED);
+    
+    if (jpeg.decodeDither(dither_space, 0))
+      {
+        time_decomp = (esp_timer_get_time() - decode_start)/1000 - time_render;
+        ESP_LOGI("decode", "%d ms - %dx%d image MCUs:%d", time_decomp, jpeg.getWidth(), jpeg.getHeight(), mcu_count);
+      } else {
+        ESP_LOGE("jpeg.decode", "Failed with error: %d", jpeg.getLastError());
+      }
+
+  } else {
+    ESP_LOGE("jpeg.openRAM", "Failed with error: %d", jpeg.getLastError());
+  }
+  jpeg.close();
+  
+  return 1;
+}
+
+// Handles Htpp events and is in charge of buffering source_buf (jpg compressed image)
+esp_err_t _http_event_handler(esp_http_client_event_t *evt)
+{
+    switch (evt->event_id)
+    {
+    case HTTP_EVENT_ERROR:
+        ESP_LOGE(TAG, "HTTP_EVENT_ERROR");
+        break;
+    case HTTP_EVENT_ON_CONNECTED:
+        ESP_LOGI(TAG, "HTTP_EVENT_ON_CONNECTED");
+        break;
+    case HTTP_EVENT_HEADER_SENT:
+        ESP_LOGI(TAG, "HTTP_EVENT_HEADER_SENT");
+        break;
+    case HTTP_EVENT_ON_HEADER:
+        #if DEBUG_VERBOSE
+          ESP_LOGI(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
+        #endif
+        break;
+    case HTTP_EVENT_ON_DATA:
+        ++countDataEventCalls;
+        #if DEBUG_VERBOSE
+          if (countDataEventCalls%10==0) {
+            ESP_LOGI(TAG, "%d len:%d\n", countDataEventCalls, evt->data_len);
+          }
+        #endif
+        
+
+        if (countDataEventCalls == 1) startTime = esp_timer_get_time();
+        // Append received data into source_buf
+        memcpy(&source_buf[img_buf_pos], evt->data, evt->data_len);
+        img_buf_pos += evt->data_len;
+
+        // Optional hexa dump
+        //ESP_LOG_BUFFER_HEX(TAG, source_buf, 100);
+        break;
+
+    case HTTP_EVENT_ON_FINISH:
+        // Do not draw if it's a redirect (302)
+        if (esp_http_client_get_status_code(evt->client) == 200) {
+
+        display.startWrite();
+          printf("%d bytes read from %s\n", img_buf_pos, IMG_URL);
+          time_download = (esp_timer_get_time()-startTime)/1000;
+
+          decodeJpeg(source_buf, 0, 0);
+          
+          ESP_LOGI("www-dw", "%d ms - download", time_download);
+          ESP_LOGI("render", "%d ms - render", time_render);
+
+          display.fillCircle(300, EPD_HEIGHT/2, 10, 50);
+          // Refresh display
+          display.endWrite();
+
+          ESP_LOGI("total", "%d ms - total time spent\n", time_download+time_decomp+time_render);
+        } else {
+          printf("HTTP on finish got status code: %d\n", esp_http_client_get_status_code(evt->client));
+        }
+        break;
+
+    case HTTP_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED\n");
+        break;
+    }
+    return ESP_OK;
+}
+
+// Handles http request
+static void http_post(void)
+{    
+    /**
+     * NOTE: All the configuration parameters for http_client must be specified
+     * either in URL or as host and path parameters.
+     */
+    esp_http_client_config_t config = {
+        .url = IMG_URL,
+        .disable_auto_redirect = false,
+        .event_handler = _http_event_handler,
+        .buffer_size = HTTP_RECEIVE_BUFFER_SIZE,
+        #if VALIDATE_SSL_CERTIFICATE == true
+        .cert_pem = (char *)server_cert_pem_start
+        #endif
+        };
+
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+
+    #if DEBUG_VERBOSE
+      printf("Free heap before HTTP download: %d\n", xPortGetFreeHeapSize());
+      if (esp_http_client_get_transport_type(client) == HTTP_TRANSPORT_OVER_SSL && config.cert_pem) {
+        printf("SSL CERT:\n%s\n\n", (char *)server_cert_pem_start);
+      }
+    #endif
+    
+    esp_err_t err = esp_http_client_perform(client);
+    if (err == ESP_OK)
+    {
+        ESP_LOGI(TAG, "\nIMAGE URL: %s\n\nHTTP GET Status = %d, content_length = %d\n",
+                 IMG_URL,
+                 esp_http_client_get_status_code(client),
+                 esp_http_client_get_content_length(client));
+    }
+    else
+    {
+        ESP_LOGE(TAG, "\nHTTP GET request failed: %s", esp_err_to_name(err));
+    }
+
+    printf("Go to sleep %d minutes\n", CONFIG_DEEPSLEEP_MINUTES_AFTER_RENDER);
+    esp_http_client_cleanup(client);
+    vTaskDelay(10);
+    deepsleep();
+}
+
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
+
+/* The event group allows multiple bits for each event, but we only care about two events:
+ * - we are connected to the AP with an IP
+ * - we failed to connect after the maximum amount of retries */
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT BIT1
+static int s_retry_num = 0;
+
+static void event_handler(void *arg, esp_event_base_t event_base,
+                          int32_t event_id, void *event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
+    {
+        esp_wifi_connect();
+    }
+    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
+    {
+        if (s_retry_num < 5)
+        {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG, "Retry to connect to the AP");
+        }
+        else
+        {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+            ESP_LOGI(TAG, "Connect to the AP failed %d times. Going to deepsleep %d minutes", 5, CONFIG_DEEPSLEEP_MINUTES_AFTER_RENDER);
+            deepsleep();
+        }
+    }
+    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
+    {
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+// Initializes WiFi the ESP-IDF way
+void wifi_init_sta(void)
+{
+    s_wifi_event_group = xEventGroupCreate();
+
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_got_ip));
+    // C++ wifi config
+    wifi_config_t wifi_config;
+    memset(&wifi_config, 0, sizeof(wifi_config));
+    wifi_config.sta.threshold.authmode = WIFI_AUTH_WPA2_PSK;
+    sprintf(reinterpret_cast<char *>(wifi_config.sta.ssid), CONFIG_ESP_WIFI_SSID);
+    sprintf(reinterpret_cast<char *>(wifi_config.sta.password), CONFIG_ESP_WIFI_PASSWORD);
+    wifi_config.sta.pmf_cfg.capable = true;
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config((wifi_interface_t)ESP_IF_WIFI_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+                                           WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+                                           pdFALSE,
+                                           pdFALSE,
+                                           portMAX_DELAY);
+
+    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually happened. */
+    if (bits & WIFI_CONNECTED_BIT)
+    {
+        ESP_LOGI(TAG, "Connected to ap SSID:%s", CONFIG_ESP_WIFI_SSID);
+    }
+    else if (bits & WIFI_FAIL_BIT)
+    {
+        ESP_LOGI(TAG, "Failed to connect to SSID:%s", CONFIG_ESP_WIFI_SSID);
+    }
+    else
+    {
+        ESP_LOGE(TAG, "UNEXPECTED EVENT");
+    }
+
+    /* The event will not be processed after unregister */
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
+    vEventGroupDelete(s_wifi_event_group);
+}
+
+void app_main() {
+
+  display.init();
+  // epd_fast:    LovyanGFX uses a 4×4 16pixel tile pattern to display a pseudo 17level grayscale.
+  // epd_quality: Uses 16 levels of grayscale
+  display.setEpdMode(epd_mode_t::epd_quality);
+
+  ep_width = EPD_WIDTH;
+  ep_height = EPD_HEIGHT;
+  printf("JPGDEC version @bitbank2\n");
+  dither_space = (uint8_t *)heap_caps_malloc(ep_width *16, MALLOC_CAP_SPIRAM);
+  if (dither_space == NULL) {
+      ESP_LOGE("main", "Initial alloc ditherSpace failed!");
+  }
+
+  // Should be big enough to allocate the JPEG file size, width * height should suffice
+  source_buf = (uint8_t *)heap_caps_malloc(ep_width * ep_height, MALLOC_CAP_SPIRAM);
+  if (source_buf == NULL) {
+      ESP_LOGE("main", "Initial alloc source_buf failed!");
+  }
+  printf("Free heap after buffers allocation: %d\n", xPortGetFreeHeapSize());
+
+  double gammaCorrection = 1.0 / gamma_value;
+  for (int gray_value =0; gray_value<256;gray_value++)
+    gamme_curve[gray_value]= round (255*pow(gray_value/255.0, gammaCorrection));
+
+  // Initialize NVS
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+  {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(ret);
+
+  // WiFi log level set only to Error otherwise outputs too much
+  esp_log_level_set("wifi", ESP_LOG_ERROR);
+  //display.clearScreen();
+  
+  // Initialization: WiFi + clean screen while downloading image
+  printf("Free heap before wifi_init_sta: %d\n", xPortGetFreeHeapSize());
+  wifi_init_sta();
+  #if VALIDATE_SSL_CERTIFICATE == true
+    obtain_time();
+  #endif
+  
+  http_post();
+}
